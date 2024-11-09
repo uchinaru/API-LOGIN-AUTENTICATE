339c75b24e437832d4bf15774aee0978a9dd51c7
Jonatas Benicio <jonathas_santos93@hotmail.com>
2024-11-09 14:56:28 -0300

    refactor: refatora o metodo de gerar token para receber o dto de usuario

--- src/main/java/login_api/utils/JwtUtils.java
+++ src/main/java/login_api/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package login_api.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -8,16 +9,21 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import login_api.models.Usuario;
+import login_api.models.UsuarioDTO;
+import login_api.repository.UsuarioRepository;
 
 @Service
 public class JwtUtils {
 
+	@Autowired
+	UsuarioRepository userRepo;
+	
 	String token;
 
-	public String gerarToken(Usuario usuario) {
+	public String gerarToken(UsuarioDTO usuario) {
 
 		try {
-			Algorithm algorithm = Algorithm.HMAC256(usuario.getToken());
+			Algorithm algorithm = Algorithm.HMAC256(usuario.getPalavraSecreta());
 
 			token = JWT.create().withIssuer(usuario.getLogin()).sign(algorithm);
 
@@ -27,14 +33,26 @@ public class JwtUtils {
 		}
 	}
 
-	public String getToken(Usuario usuario) {
-		Algorithm algorithm = Algorithm.HMAC256(usuario.getToken());
-		try {
-			token = JWT.require(algorithm).withIssuer(usuario.getLogin()).build().verify(usuario.getToken())
-					.getSubject();
-			return token;
-		} catch (Exception e) {
-			throw new JWTVerificationException("Token inválido ou expirado!");
+	public String getToken(UsuarioDTO usuarioDTO) {
+		Algorithm algorithm = Algorithm.HMAC256(usuarioDTO.getPalavraSecreta());
+		
+		Usuario user = userRepo.findByLogin(usuarioDTO.getLogin());
+		
+		if(user != null) {
+			
+			try {
+				token = JWT.require(algorithm)
+						.withIssuer(user.getLogin()).build()
+						.verify(user.getToken())
+						.getSubject();
+				
+				return token;
+			} catch (Exception e) {
+				throw new JWTVerificationException("Token inválido ou expirado!");
+			}
+		}else {
+			return null;
 		}
+
 	}
 }
